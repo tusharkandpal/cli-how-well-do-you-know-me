14e962755206debc1b178901081c8fe13f31f0fc
Tushar Kandpal <kandpal.tushar@gmail.com>
2021-09-05 14:55:33 +0530

    initial commit
    created index.js & README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# cli-how-well-you-know-me
- CLI Quiz App
+# how-well-you-know-me?
+CLI Quiz App
+
+## How to play: 
+1. At first, please enter your name.
+1. Answer the questions based on Tushar (me).
+1. For each correct answer you will get 1 point.
+1. No negative points for wrong answer.
+
+## Start the quiz: [How Well Do You Know Me](https://replit.com/@TusharKandpal/CLI-App-How-Well-Do-You-Know-ME?embed=1output=1#index.js)
---
+++ index.js
@@ -0,0 +1,57 @@
+var readLineSync = require("readline-sync");
+
+var score = 0;
+
+var userName = readLineSync.question("Please enter your name ? ");
+console.log("hello " + userName + "! Let's start the quiz...");
+
+console.log("Answer in one word.");
+console.log("--------------------------");
+
+questions = [
+  {
+    que: "Where do I live ? ",
+    ans: "Nainital"
+  },
+  {
+    que: "What is my nickname ? ",
+    ans: "Kush"
+  },
+  {
+    que: "Am I a marvel fan ? ",
+    ans: "Yes"
+  },
+  {
+    que: "In which month do my birthday lie ? ",
+    ans: "April"
+  }
+]
+
+function quiz(question, answer) {
+
+  var userAns = readLineSync.question(question);
+
+  if (userAns.toLowerCase() == answer.toLowerCase())
+  {
+    score = score + 1;
+    console.log("Yay Right !! Current Score: "+ score);
+  }
+  else
+  {
+    console.log("Wrong !!");
+  }
+
+}
+
+for (var i = 0; i < questions.length; i++)
+{
+  var que = questions[i].que;
+  var ans = questions[i].ans;
+
+  quiz(que, ans);
+  
+  console.log("--------------------------");
+}
+
+console.log("Your final score is: " + score);
+
